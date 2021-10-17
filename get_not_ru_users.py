ca9478a0313ab03b70176394682a73c96fa13243
bret99 <64787351+bret99@users.noreply.github.com>
2021-10-17 10:05:16 +0000

    Update get_not_ru_users.py

--- get_not_ru_users.py
+++ get_not_ru_users.py
@@ -7,7 +7,7 @@ IPs_list = []
 
 os.system(
     "cat /PATH_TO_OPENVPN_LOG_FILE | awk '{print $1}' | head -n -3 | tail -n +4 | sed 's/ROUTING//;s/Virtual//' | sed '/^$/d' > /tmp/not_ru_users.txt"
-)   
+)
 
 with open('/tmp/not_ru_users.txt') as conns:
     for line in conns.readlines():
@@ -24,16 +24,8 @@ except (ValueError, IndexError):
 
 def get_IP_info(ip):
     url = 'https://api.abuseipdb.com/api/v2/check'
-
     querystring = {'ipAddress': ip, 'maxAgeInDays': '90'}
-
-    headers = {
-        'Accept':
-        'application/json',
-        'Key':
-        'YOUR_ABUSEIPDB_API_KEY'
-    }
-
+    headers = {'Accept': 'application/json', 'Key': 'YOUR_ABUSEIPDB_API_KEY'}
     response = requests.request(method='GET',
                                 url=url,
                                 headers=headers,
@@ -42,9 +34,13 @@ def get_IP_info(ip):
     decodedResponse = json.loads(response.text)
     if decodedResponse['data']['countryCode'] != 'RU':
         print("\033[1;91m")
-        print(json.dumps(decodedResponse['data']['ipAddress'], sort_keys=True, indent=4),"\033[1;00m")
+        print(
+            json.dumps(decodedResponse['data']['ipAddress'],
+                       sort_keys=True,
+                       indent=4), "\033[1;00m")
         not_ru_IP = decodedResponse['data']['ipAddress']
-        os.system("grep {} /PATH_TO_OPENVPN_LOG_FILE | head -n 1".format(not_ru_IP))
+        os.system(
+            "grep {} /PATH_TO_OPENVPN_LOG_FILE | head -n 1".format(not_ru_IP))
         print("\033[1;94mIP info: \033[1;00m")
         print(json.dumps(decodedResponse['data'], sort_keys=True, indent=4))
         print("")
@@ -54,4 +50,3 @@ for line in IPs_list:
     get_IP_info(line)
 
 os.system('rm /tmp/not_ru_users.txt')
-
