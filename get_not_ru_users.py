f28acdfb64fc009ffa2be465952e18eda7188139
bret99 <64787351+bret99@users.noreply.github.com>
2022-03-02 14:07:34 +0400

    Made output more pretty

--- get_not_ru_users.py
+++ get_not_ru_users.py
@@ -9,6 +9,7 @@ os.system(
     "cat /PATH_TO_OPENVPN_LOG_FILE | awk '{print $1}' | head -n -3 | tail -n +4 | sed 's/ROUTING//;s/Virtual//' | sed '/^$/d' > /tmp/not_ru_users.txt"
 )
 
+print("\033[1;90mSearching...\033[1;00m")
 with open('/tmp/not_ru_users.txt') as conns:
     for line in conns.readlines():
         conns_list.append(line.split(','))
